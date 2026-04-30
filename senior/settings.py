48e1d1a899583b9ade3555e24986e163aac9c225
Dovudbek <dovudbek@Dovudbeks-MacBook-Air.local>
2026-04-30 18:40:51 +0500

    setting file updated

--- senior/settings.py
+++ senior/settings.py
@@ -86,16 +86,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tashkent"
 USE_I18N = True
 USE_TZ = True
 
 
 # Static / media
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
